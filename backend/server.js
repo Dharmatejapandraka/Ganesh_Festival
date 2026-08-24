7761a277feb7e3c2f0663681404549d6687b533b
Pandraka Dharmateja <pandrakadharmateja@gmail.com>
2026-08-24 10:07:35 +0530

    Configure server for Render

--- backend/server.js
+++ backend/server.js
@@ -286,17 +286,8 @@ app.use(
 // SERVER
 // =====================================================
 
-const PORT =
-  process.env.PORT || 5000;
+const PORT = process.env.PORT || 5000;
 
-
-app.listen(
-  PORT,
-  () => {
-
-    console.log(
-      `Server running on http://localhost:${PORT}`
-    );
-
-  }
-);
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`Server running on port ${PORT}`);
+});
